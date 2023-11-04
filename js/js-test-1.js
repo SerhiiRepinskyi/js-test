798009679eff0fdeb3c2550b8d112a64cd3d7b46
Serhii Repinskyi <115186821+serhiirepinskyi@users.noreply.github.com>
2023-11-05 01:15:26 +0200

    task-3

--- js/js-test-1.js
+++ js/js-test-1.js
@@ -30,11 +30,39 @@
 // ***** ЗАВДАННЯ 2 *****
 // Напиши функцію, яка приймає довільний рядок і повертає рядок у якому символи переданого рядка розміщені в алфавітному порядку.
 
+// function StringChallenge(str) {
+//   const sortStr = str.split("").sort().join("");
+//   return sortStr;
+// }
+
+// console.log(StringChallenge("result")); // "elrstu"
+// console.log(StringChallenge("результат")); // "аезлрттуь"
+// console.log(StringChallenge("Hello, world!")); // " !,Hdellloorw"
+
+// ***** ЗАВДАННЯ 3 *****
+// Напиши функцію StringChallenge(str), що має прочитати переданий параметр str, який буде містити великий рядок буквено-цифрових символів з однією зірочкою, що розділяє рядок порівну на два окремі рядки. Ваша мета повернути новий рядок, поєднавши символи у відповідних місцях обох рядків.
+// Наприклад: якщо передається рядок "abc1*kyoo", то функція повинна повернути рядок "akbyco1o", тому що а поєднується з k, b - з у і т.д. Рядок завжди буде ділитися порівну зірочкою в центрі.
+
 function StringChallenge(str) {
-  const sortStr = str.split("").sort().join("");
-  return sortStr;
+  const parts = str.split("*");
+  const leftPart = parts[0].split("");
+  const rightPart = parts[1].split("");
+
+  const maxLength = Math.max(leftPart.length, rightPart.length);
+
+  let result = "";
+
+  for (let i = 0; i < maxLength; i++) {
+    if (leftPart[i]) {
+      result += leftPart[i];
+    }
+    if (rightPart[i]) {
+      result += rightPart[i];
+    }
+  }
+
+  return result;
 }
 
-console.log(StringChallenge("result")); // "elrstu"
-console.log(StringChallenge("результат")); // "аезлрттуь"
-console.log(StringChallenge("Hello, world!")); // " !,Hdellloorw"
+console.log(StringChallenge("abc1*kyoo")); // "akbyco1o"
+console.log(StringChallenge("123gh*abcde")); // "1a2b3cgdhe"
