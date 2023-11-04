3372d0ef93c2cb0395d91a11d68d3b5417f09194
Serhii Repinskyi <115186821+serhiirepinskyi@users.noreply.github.com>
2023-11-05 01:19:31 +0200

    task-4

--- js/js-test-1.js
+++ js/js-test-1.js
@@ -43,26 +43,60 @@
 // Напиши функцію StringChallenge(str), що має прочитати переданий параметр str, який буде містити великий рядок буквено-цифрових символів з однією зірочкою, що розділяє рядок порівну на два окремі рядки. Ваша мета повернути новий рядок, поєднавши символи у відповідних місцях обох рядків.
 // Наприклад: якщо передається рядок "abc1*kyoo", то функція повинна повернути рядок "akbyco1o", тому що а поєднується з k, b - з у і т.д. Рядок завжди буде ділитися порівну зірочкою в центрі.
 
-function StringChallenge(str) {
-  const parts = str.split("*");
-  const leftPart = parts[0].split("");
-  const rightPart = parts[1].split("");
+// function StringChallenge(str) {
+//   const parts = str.split("*");
+//   const leftPart = parts[0].split("");
+//   const rightPart = parts[1].split("");
 
-  const maxLength = Math.max(leftPart.length, rightPart.length);
+//   const maxLength = Math.max(leftPart.length, rightPart.length);
 
-  let result = "";
+//   let result = "";
 
-  for (let i = 0; i < maxLength; i++) {
-    if (leftPart[i]) {
-      result += leftPart[i];
+//   for (let i = 0; i < maxLength; i++) {
+//     if (leftPart[i]) {
+//       result += leftPart[i];
+//     }
+//     if (rightPart[i]) {
+//       result += rightPart[i];
+//     }
+//   }
+
+//   return result;
+// }
+
+// console.log(StringChallenge("abc1*kyoo")); // "akbyco1o"
+// console.log(StringChallenge("123gh*abcde")); // "1a2b3cgdhe"
+
+// ***** ЗАВДАННЯ 4 *****
+// Нехай функція ArrayChallenge(arr) візьме масив чисел, що зберігається в arr, і поверне рядок true, якщо будь-яка комбінація чисел у масиві (за винятком найбільшого числа) може бути складена так, щоб вона дорівнювала найбільшому числу в масиві, інакше false.
+// Наприклад: якщо arr містить [4, 6, 23, 10, 1, 3], функція повинна повернути true, тому що 4+6+10+3=23. Масив не може буди порожнім, не буде містити всіх однакових елементів, і може мати відємні числа.
+
+function ArrayChallenge(arr) {
+  // Сортуємо масив у спадаючому порядку
+  arr.sort((a, b) => b - a);
+
+  // Видаляємо найбільше число
+  const maxNumber = arr.shift();
+
+  function canAddToMax(target, numbers) {
+    if (target === 0) {
+      return true; // Ціль досягнута
     }
-    if (rightPart[i]) {
-      result += rightPart[i];
+    if (numbers.length === 0) {
+      return false; // Неможливо досягнути ціль
     }
+
+    // Спробувати додати поточне число або пропустити його
+    return (
+      canAddToMax(target - numbers[0], numbers.slice(1)) ||
+      canAddToMax(target, numbers.slice(1)) ||
+      canAddToMax(target + numbers[0], numbers.slice(1)) // Додавання враховує від'ємні числа
+    );
   }
 
-  return result;
+  return canAddToMax(maxNumber, arr);
 }
 
-console.log(StringChallenge("abc1*kyoo")); // "akbyco1o"
-console.log(StringChallenge("123gh*abcde")); // "1a2b3cgdhe"
+console.log(ArrayChallenge([4, 6, 23, 10, 1, 3])); // true
+console.log(ArrayChallenge([5, 7, 16, 1, 2])); // false
+console.log(ArrayChallenge([3, 5, -1, 8, 12])); // true
