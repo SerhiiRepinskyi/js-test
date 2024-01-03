bbaee8e3862859132d364b30280aa43effba721b
Serhii Repinskyi <115186821+serhiirepinskyi@users.noreply.github.com>
2024-01-04 00:08:16 +0200

    lesson-4: task 2.1

--- js/lessons/lesson-4.js
+++ js/lessons/lesson-4.js
@@ -1,34 +1,69 @@
 // ЗАДАЧА 1
 // Створіть карточки товару з масиву використовуючи createElement.
 
-import cars from "../data/cars.js";
+// import cars from "../data/cars.js";
 
-const carsCardEl = document.querySelector(".js-goods");
-console.log(carsCardEl);
+// const carsCardEl = document.querySelector(".js-goods");
+// console.log(carsCardEl);
 
-const getCard = ({ name, img, price, description }) => {
-  const cardEl = document.createElement("li");
+// const getCard = ({ name, img, price, description }) => {
+//   const cardEl = document.createElement("li");
 
-  const nameEl = document.createElement("h2");
-  nameEl.textContent = name;
+//   const nameEl = document.createElement("h2");
+//   nameEl.textContent = name;
 
-  const pictureEl = document.createElement("img");
-  pictureEl.src = img;
-  pictureEl.alt = name;
-  pictureEl.width = 300;
+//   const pictureEl = document.createElement("img");
+//   pictureEl.src = img;
+//   pictureEl.alt = name;
+//   pictureEl.width = 300;
 
-  const priceEl = document.createElement("p");
-  priceEl.textContent = price;
+//   const priceEl = document.createElement("p");
+//   priceEl.textContent = price;
 
-  const descrEl = document.createElement("p");
-  descrEl.textContent = description;
+//   const descrEl = document.createElement("p");
+//   descrEl.textContent = description;
 
-  cardEl.append(nameEl, pictureEl, priceEl, descrEl);
-  console.log(cardEl);
+//   cardEl.append(nameEl, pictureEl, priceEl, descrEl);
+//   console.log(cardEl);
 
-  return cardEl;
+//   return cardEl;
+// };
+
+// const keys = cars.map(getCard);
+
+// carsCardEl.append(...keys);
+
+// ЗАДАЧА 2
+// 2.1 НАПИШІТЬ КАЛЬКУЛЯТОР, ЯКИЙ БУДЕ РАХУВАТИ ЦІНУ ШАШЛИКА, В ЗАЛЕЖНОСТІ ВІД ЦІНИ ЗА КІЛОГРАМ І КІЛЬКОСТІ КУПЛЕНОГО ПРОДУКТА, І БУДЕ ВИВОДИТИ РЕЗУЛЬТАТ НА ЕКРАН.
+
+const refs = {
+  priceEl: document.querySelector("#price"),
+  quantityEl: document.querySelector("#quantity"),
+  totalEl: document.querySelector(".total"),
+  formEl: document.querySelector(".form"),
+  amountEl: document.querySelector(".amount"),
+  formCont: document.querySelector(".container"),
 };
 
-const keys = cars.map(getCard);
+// console.log(refs);
 
-carsCardEl.append(...keys);
+function calculator(e) {
+  e.preventDefault();
+  const price = refs.priceEl.value;
+  const quantity = refs.quantityEl.value;
+
+  const total = price * quantity;
+
+  refs.totalEl.textContent = total;
+
+  refs.amountEl.textContent = quantity;
+
+  // return total;
+}
+
+// calculator();
+// console.log(calculator());
+window.addEventListener("DOMContentLoaded", calculator); // при завантаженні сторінки - запустити calculator
+
+refs.formEl.addEventListener("input", calculator);
+// refs.formEl.addEventListener("submit", calculator);
