4273c8f46e3e53a56a349267e1307fff445164ef
Serhii Repinskyi <115186821+serhiirepinskyi@users.noreply.github.com>
2024-02-17 21:54:04 +0200

    lesson-6: tasks 3.1-3.2

--- js/lessons/lesson-6.js
+++ js/lessons/lesson-6.js
@@ -31,30 +31,108 @@
 // ВИКОРИСТОВУЮЧИ API ДАНОГО ПОСИЛАННЯ https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${userValue} ,
 // ТА ЗА ДОПОМОГОЮ fetch, ВИВЕДИ НА ЕКРАН ОТРИМАНИЙ РЕЗУЛЬТАТ (ПАРАМЕТРИ ЗАПИТУ ОТРИМАЙ З INPUT)
 
+// const form = document.querySelector("#form");
+// const input = document.querySelector("#input");
+// const wrapper = document.querySelector(".render");
+
+// form.addEventListener("submit", onSubmit);
+
+// function onSubmit(event) {
+//   event.preventDefault();
+
+//   const userValue = input.value;
+
+//   fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${userValue}`)
+//     .then((responce) => responce.json())
+//     .then((data) => sortRender(data.drinks))
+//     .catch((err) => console.log(err));
+// }
+
+// function cardElem({ strDrink, strDrinkThumb, strCategory }) {
+//   const murkUp = `<h2>${strDrink}</h2>
+//   <img src="${strDrinkThumb}" alt="${strDrink}">
+//   <p>${strCategory}</p>`;
+//   wrapper.insertAdjacentHTML("beforeend", murkUp);
+// }
+
+// function sortRender(elements) {
+//   elements.forEach((element) => cardElem(element));
+// }
+
+// ========== ЗАДАЧА 3 ==========
+// НАПИШІТЬ ПАГІНАЦІЮ, ДЛЯ ПЕРЕХОДУ ПО СТОРІНКАХ
+// https://docs.github.com/en/rest/search?apiVersion=2022-11-28#search-user
+
+// Задача 3.1 - search users
+// fetch(`https://api.github.com/search/users?q=${}&client_id=67684cabc84f94f0938e&client_secret=782ea639550c1b5d986bdd8129813652ed04c92c`)
+
+// const form = document.querySelector("#form");
+// const input = document.querySelector("#input");
+// const render = document.querySelector(".render");
+// const loadMoreBtn = document.querySelector(".load-more");
+// let count = 1;
+
+// form.addEventListener("submit", handleFormSubmit);
+// loadMoreBtn.addEventListener("click", handleFormSubmit);
+
+// function handleFormSubmit(e) {
+//   e.preventDefault();
+//   const inputValue = input.value;
+
+//   fetch(
+//     `https://api.github.com/search/users?q=${inputValue}&client_id=67684cabc84f94f0938e&client_secret=782ea639550c1b5d986bdd8129813652ed04c92c&per_page=5&page=${count}`
+//   )
+//     .then((data) => data.json())
+//     .then((data) => renderMarkup(data.items))
+//     .then(() => (count += 1))
+//     .catch((err) => console.log(err));
+// }
+
+// function renderMarkup(elements) {
+//   const elementMarkup = elements
+//     .map(
+//       ({ login, avatar_url }) =>
+//         `<h2>${login}</h2><img src="${avatar_url}" alt="${login}">`
+//     )
+//     .join("");
+//   render.insertAdjacentHTML("beforeend", elementMarkup);
+// }
+
+// Задача 3.2 - search repositories
+// https://api.github.com/search/repositories?q=Q
+
 const form = document.querySelector("#form");
 const input = document.querySelector("#input");
-const wrapper = document.querySelector(".render");
+const render = document.querySelector(".render");
+const loadMoreBtn = document.querySelector(".load-more");
+const perPage = 5;
+let page = 1;
 
-form.addEventListener("submit", onSubmit);
+form.addEventListener("submit", handleForm);
+loadMoreBtn.addEventListener("click", handleForm);
 
-function onSubmit(event) {
-  event.preventDefault();
+function handleForm(e) {
+  e.preventDefault();
+  const inputValue = input.value;
 
-  const userValue = input.value;
-
-  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${userValue}`)
-    .then((responce) => responce.json())
-    .then((data) => sortRender(data.drinks))
+  fetch(
+    `https://api.github.com/search/repositories?q=${inputValue}&client_id=67684cabc84f94f0938e&client_secret=782ea639550c1b5d986bdd8129813652ed04c92c&per_page=${perPage}&page=${page}`
+  )
+    .then((data) => data.json())
+    .then((data) => renderMarkup(data.items))
+    .then(() => (page += 1))
     .catch((err) => console.log(err));
 }
 
-function cardElem({ strDrink, strDrinkThumb, strCategory }) {
-  const murkUp = `<h2>${strDrink}</h2>
-  <img src="${strDrinkThumb}" alt="${strDrink}">
-  <p>${strCategory}</p>`;
-  wrapper.insertAdjacentHTML("beforeend", murkUp);
-}
-
-function sortRender(elements) {
-  elements.forEach((element) => cardElem(element));
+function renderMarkup(elements) {
+  const elementMarkup = elements
+    .map(
+      ({ name, owner, homepage }) => `
+      <h2>${name}</h2>
+      <p>Owner: ${owner.login}</p>
+      <p>Homepage: <a href=${homepage}>${name}</a></p>
+      `
+    )
+    .join("");
+  render.insertAdjacentHTML("beforeend", elementMarkup);
 }
